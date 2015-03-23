dfba986ac276ecf7d05be7bd240fc4683fb1c1bc
pmartens <pieter@cg.nl>
2015-03-23 11:20:34 +0100

    add options for debuging

--- lib/ecircle_soap_api/base.rb
+++ lib/ecircle_soap_api/base.rb
@@ -1,10 +1,12 @@
 module EcircleSoapApi
   class Base
     extend ::Savon::Model
-    
+
     client wsdl: EcircleSoapApi.wsdl
     global :open_timeout, EcircleSoapApi.open_timeout
     global :read_timeout, EcircleSoapApi.read_timeout
     global :basic_auth, EcircleSoapApi.username, EcircleSoapApi.password
+    # global :log_level, :debug
+    # global :log, true
   end
 end
